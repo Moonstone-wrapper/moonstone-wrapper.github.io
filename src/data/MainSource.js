aafd58ddc65e86d0d19ee6e88567f6de44fdb1c8
Checkium <checkium@gmail.com>
2021-05-15 19:01:45 +0100

    Fix global

--- src/data/MainSource.js
+++ src/data/MainSource.js
@@ -4,7 +4,7 @@ const branchBlacklist = new Set(['docs', 'webpack', 'v8']);
 export default new DocsSource({
   id: 'main',
   name: 'Main library',
-  global: 'Dogehouse',
+  global: 'Moonstone',
   repo: 'fuwwy/moonstone',
   defaultTag: 'main',
   branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
